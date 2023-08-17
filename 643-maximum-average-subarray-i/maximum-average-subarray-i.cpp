d45d9092c53024ec68e57a0be39c46a10b8edf0a
Silver Dime <77194774+Shrey-Dubey-25@users.noreply.github.com>
2023-08-18 01:13:23 +0530

    Time: 136 ms (84.99%) | Memory: 109.6 MB (34.24%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& v, int k) {
+        int s=0,m=INT_MIN;
+        for(int i=0;i<k;i++){
+            s+=v[i];
+        }
+        m=s;
+        int i=0,j=k;
+        while(j<v.size()){
+            s+=v[j];
+            s-=v[i];
+            if(s>=m){
+                m=s;
+            }
+            j++;
+            i++;
+        }
+        return (double)m/(double)k; 
+}
+};
