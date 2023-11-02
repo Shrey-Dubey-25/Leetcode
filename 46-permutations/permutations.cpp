10fc00dba42f81890cb77476e6794313cf30a1da
Silver Dime <77194774+Shrey-Dubey-25@users.noreply.github.com>
2023-11-03 01:42:12 +0530

    Time: 6 ms (17.40%) | Memory: 9 MB (8.87%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    void helper(vector<int>&v,vector<int>&f,vector<int>&p,vector<vector<int>>&ans){
-        if(p.size()==v.size()){
-            ans.push_back(p);
+void helper(vector<int>&v,vector<int>temp,vector<bool>vis,vector<vector<int>>&ans){
+        if(temp.size()==v.size()){
+            ans.push_back(temp);
             return;
         }
         for(int i=0;i<v.size();i++){
-            if(f[i]==0){
-                p.push_back(v[i]);
-                f[i]=1;
-                helper(v,f,p,ans);
-                f[i]=0;
-                p.pop_back();
+            if(vis[i]==0){
+                temp.push_back(v[i]);
+                vis[i]=1;
+                helper(v,temp,vis,ans);
+                vis[i]=0;
+                temp.pop_back();
             }
         }
-    }
-    vector<vector<int>> permute(vector<int>& v) {
-        vector<int>f(v.size(),0);
-        vector<int>p;
-        vector<vector<int>>ans;
-        helper(v,f,p,ans);
-        return ans;
-    }
+
+}
+vector<vector<int>> permute(vector<int>& v) {
+    vector<vector<int>>ans;
+    vector<bool>vis(v.size(),0);
+    helper(v,{},vis,ans);
+    return ans;
+}
 };
