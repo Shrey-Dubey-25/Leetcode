d8102d7ec9dfc1eece8610e63ff65200e6300cdf
Silver Dime <77194774+Shrey-Dubey-25@users.noreply.github.com>
2023-12-02 19:29:19 +0530

    Time: 385 ms (7.93%) | Memory: 17.1 MB (19.31%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -1,19 +1,36 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+ * Definition for singly-linked list.*/
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode(int x) : val(x), next(NULL) {}
+//  };
+ 
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){
-        ListNode *ptrA = headA, *ptrB = headB;
-    while (ptrA != ptrB) { 
-        ptrA = ptrA ? ptrA->next : headB;
-        ptrB = ptrB ? ptrB->next : headA;
+    ListNode *getIntersectionNode(ListNode *h1, ListNode *h2) {
+    vector<ListNode *>a;
+    vector<ListNode *>b;
+    if(h1==NULL|| h2==NULL){
+        return NULL;
     }
-    return ptrA;
+    ListNode *p1=h1,*p2=h2;
+    while(p1!=NULL){
+        a.push_back(p1);
+        p1=p1->next;
     }
+    while(p2!=NULL){
+        b.push_back(p2);
+        p2=p2->next;
+    }
+
+    for(auto i:a){
+        for(auto j:b){
+            if(j==i){
+                return j;
+            }
+        }
+    }   
+    return NULL;
+}
 };
